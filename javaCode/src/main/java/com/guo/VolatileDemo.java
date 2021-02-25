3274de0faa55e559ea0f53c7b0b3fe9c4b4f3417
guoyuan_ai <guoyuan@123>
2021-02-25 20:40:18 +0800

    VolatileDemo

---
+++ javaCode/src/main/java/com/guo/VolatileDemo.java
@@ -0,0 +1,43 @@
+package com.guo;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * volatileDemo
+ */
+public class VolatileDemo {
+
+    static class MyDate{
+        volatile int number=0;
+
+        public void add()
+        {
+            number++;
+        }
+
+        AtomicInteger atomicInteger=new AtomicInteger();
+
+        public  void addAtomic() {
+             atomicInteger.getAndIncrement();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        MyDate myDate=new MyDate();
+        for (int i = 1; i <=20 ; i++) {
+            new Thread(()->{
+                for (int j = 1; j <=1000; j++) {
+                    myDate.add();
+                    myDate.addAtomic();
+                }
+            },String.valueOf(i)).start();
+        }
+        while (Thread.activeCount()>2)
+        {
+            Thread.yield();
+        }
+        System.out.println(Thread.currentThread().getName()+"\t int type  number value: "+ myDate.number);
+        System.out.println(Thread.currentThread().getName()+"\t atomicInteger type number value: "+ myDate.atomicInteger);
+    }
+}
